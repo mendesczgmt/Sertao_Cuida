f32893f762bb1884b287ce4204b9a2dd9b62b7cf
Gabryel Mendes <gabryelmendecz@gmail.com>
2023-09-05 14:46:32 -0300

    criando a api

---
+++ Web/Backend/SertaoCuidado/src/main/java/serdaoCuidado/SertaoCuidado/SertaoCuidadoApplication.java
@@ -0,0 +1,13 @@
+package serdaoCuidado.SertaoCuidado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SertaoCuidadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SertaoCuidadoApplication.class, args);
+	}
+
+}
---
+++ Web/Backend/SertaoCuidado/src/test/java/serdaoCuidado/SertaoCuidado/SertaoCuidadoApplicationTests.java
@@ -0,0 +1,13 @@
+package serdaoCuidado.SertaoCuidado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SertaoCuidadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
