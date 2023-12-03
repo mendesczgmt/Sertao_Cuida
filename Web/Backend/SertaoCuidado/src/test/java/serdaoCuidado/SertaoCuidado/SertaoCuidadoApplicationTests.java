2077de1c471100d2364b481083b646f8ad5a5f79
Pablo <pablocraftipu@gmail.com>
2023-12-03 15:58:31 -0300

    Testes Pacientes

--- Web/Backend/SertaoCuidado/src/test/java/serdaoCuidado/SertaoCuidado/SertaoCuidadoApplicationTests.java
+++ Web/Backend/SertaoCuidado/src/test/java/serdaoCuidado/SertaoCuidado/SertaoCuidadoApplicationTests.java
@@ -10,29 +10,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class SertaoCuidadoApplicationTests {
 
-	@Test
-	public void testEndereco() {
-		DadosEndereco dados = new DadosEndereco("Patos", "BH", "Rua NaoSei", 123);
-		Endereco endereco = new Endereco(dados);
+    @Test
+    public void testEndereco() {
+        DadosEndereco dados = new DadosEndereco("Patos", "BH", "Rua NaoSei", 123);
+        Endereco endereco = new Endereco(dados);
 
-		assertEquals("Patos", endereco.getCidade());
-		assertEquals("BH", endereco.getBairro());
-		assertEquals("Rua NaoSei", endereco.getLogradouro());
-		assertEquals(123, endereco.getNumero());
-	}
+        assertEquals("Patos", endereco.getCidade());
+        assertEquals("BH", endereco.getBairro());
+        assertEquals("Rua NaoSei", endereco.getLogradouro());
+        assertEquals(123, endereco.getNumero());
+    }
 
-	@Test
-	public void testAtualizarInformacoes() {
-		Endereco endereco = new Endereco("Patos", "BH", "Rua NaoSei", 456);
+    @Test
+    public void testAtualizarInformacoes() {
+        Endereco endereco = new Endereco("Patos", "BH", "Rua NaoSei", 456);
 
-		DadosEndereco novosDados = new DadosEndereco("Sao Mamede", null, "Rua Lins", 789);
-		endereco.atualizarInformacoes(novosDados);
+        DadosEndereco novosDados = new DadosEndereco("Sao Mamede", null, "Rua Lins", 789);
+        endereco.atualizarInformacoes(novosDados);
 
-		assertEquals("Sao Mamede", endereco.getCidade());
-		assertEquals("?", endereco.getBairro()); // O bairro não foi alterado nos novos dados
-		assertEquals("Rua Lins", endereco.getLogradouro());
-		assertEquals(789, endereco.getNumero());
-	}
+        assertEquals("Sao Mamede", endereco.getCidade());
+        assertEquals("?", endereco.getBairro()); // O bairro não foi alterado nos novos dados
+        assertEquals("Rua Lins", endereco.getLogradouro());
+        assertEquals(789, endereco.getNumero());
+    }
 }
 
 
